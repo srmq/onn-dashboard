498ec47e70b9fa46526eaffaeff8dc99960ce645
Sergio Queiroz <srmq@cin.ufpe.br>
2022-10-23 12:18:26 -0300

    workinprogress on OnnData

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Topbar from './components/topbar/Topbar';
+import OnnData from './util/onnData';
 import { fetchONNData } from './services/w3storage';
 
 function App() {
+    const [onnData, setOnnData] = useState<OnnData>({});
 
-  fetchONNData().then( onnIndexData => {
-    console.log(onnIndexData);
-  });
+    useEffect(() => {
+        fetchONNData().then(onnIndexData => {
+            let onnData = onnIndexData as OnnData;
+            console.log(onnData);
+            setOnnData(onnData);
+        });
+    }, []);
 
-  return (
-    <div>
-      <Topbar />
-    </div>
-  );
+    return (
+        <div>
+            <Topbar onnData={onnData} />
+        </div>
+    );
 }
 
 export default App;
